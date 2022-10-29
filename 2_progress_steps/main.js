1f9ce6b9e0f6a5e04ef33d32d533f6b07923bc2f
Larkuo <larkuowt@gmail.com>
2022-10-29 19:43:21 +0000

    added js/logic for project 2

---
+++ 2_progress_steps/main.js
@@ -0,0 +1,69 @@
+function handleNext () {
+    const num_ids = ['num-1', 'num-2', 'num-3', 'num-4'];
+    const bar_ids = ['bar-1', 'bar-2', 'bar-3'];
+
+    let active_arr  = document.getElementsByClassName('num-active');
+    let arr_len = active_arr.length;
+
+    let next_button = document.getElementById('next-btn');
+    let prev_button = document.getElementById('prev-btn');
+
+    if(arr_len < 4){
+        let next_num = document.getElementById(num_ids[arr_len]);
+        let next_bar = document.getElementById(bar_ids[arr_len - 1]);
+
+        next_bar.className = 'bar-active';
+        next_num.className = 'num-active';
+
+    }
+
+    active_arr = document.getElementsByClassName('num-active');
+    arr_len = active_arr.length;
+
+    if(arr_len < 4 && arr_len > 1){
+        prev_button.className = 'btn-active';
+        prev_button.disabled = false;
+    }else if(arr_len === 4){
+        next_button.className = 'btn-inactive';
+        next_button.disabled = true;
+
+        let prev_button = document.getElementById('prev-btn');
+        prev_button.className = 'btn-active';
+        prev_button.disabled = false;
+    }
+}
+
+function handlePrev () {
+    const num_ids = ['num-1', 'num-2', 'num-3', 'num-4'];
+    const bar_ids = ['bar-1', 'bar-2', 'bar-3'];
+
+    let active_arr  = document.getElementsByClassName('num-active');
+    let arr_len = active_arr.length;
+
+    let next_button = document.getElementById('next-btn');
+    let prev_button = document.getElementById('prev-btn');
+
+    if(arr_len < 4 && arr_len > 1){
+        prev_button.className = 'btn-active';
+        prev_button.disabled = false;
+    }
+
+    if(arr_len > 1){
+        let prev_num = document.getElementById(num_ids[arr_len-1]);
+        let prev_bar = document.getElementById(bar_ids[arr_len - 2]);
+
+        prev_bar.className = 'bar-inactive';
+        prev_num.className = 'num-inactive';
+    }
+
+    active_arr = document.getElementsByClassName('num-active');
+    arr_len = active_arr.length;
+
+    if(arr_len === 1){
+        next_button.className = 'btn-active';
+        next_button.disabled = false;
+
+        prev_button.className = 'btn-inactive';
+        prev_button.disabled = true;
+    }
+}
