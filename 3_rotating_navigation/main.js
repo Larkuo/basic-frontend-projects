45d05d9a3a36477ecee2546761fa56d367c11167
Larkuo <larkuowt@gmail.com>
2022-11-22 17:38:31 +0000

    project 3 everything works now

--- 3_rotating_navigation/main.js
+++ 3_rotating_navigation/main.js
@@ -1,3 +1,21 @@
+function moveMenuButtons(rotate){
+    const menu_dict_arr = [
+        {id: 'home-button', yes: 'home-yes-rotate', no: 'home-no-rotate'},
+        {id: 'about-button', yes: 'about-yes-rotate', no: 'about-no-rotate'},
+        {id: 'contact-button', yes: 'contact-yes-rotate', no: 'contact-no-rotate'}
+    ]
+    
+    menu_dict_arr.map(dict => {
+        let element = document.getElementById(dict.id);
+        if(rotate) {
+            element.className = "menu-label " + dict.yes;
+        }else {
+            element.className = "menu-label " + dict.no;
+        }
+    });
+    
+}
+
 function onClickMenu() {
     let semi_circle = document.getElementById('rotate-div');
     let main_page = document.getElementById('white-div-main');
@@ -5,9 +23,11 @@ function onClickMenu() {
     if(semi_circle.className === 'yes-rotate'){
         semi_circle.className = 'no-rotate';
         main_page.className = 'no-rotate-white-div';
+        moveMenuButtons(false);
     }else {
         semi_circle.className = 'yes-rotate';
         main_page.className = 'yes-rotate-white-div';
+        moveMenuButtons(true);
     }
 
 }
